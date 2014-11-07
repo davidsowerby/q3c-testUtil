07eedd96202486b0c36b391e61d158df08d9f110
David Sowerby <david.sowerby@virgin.net>
2014-11-07 15:18:59 +0000

    Pages renamed to uk.q3c.krail from uk.co.v7

--- src/main/java/uk/q3c/krail/testutil/TestResource.java
+++ src/main/java/uk/q3c/krail/testutil/TestResource.java
@@ -1,14 +1,17 @@
 /*
  * Copyright (c) 2014 David Sowerby
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the License.
  */
 
-package uk.co.q3c.v7.testutil;
+package uk.q3c.krail.testutil;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -18,8 +21,8 @@ import java.nio.file.Paths;
  * A couple of helper methods to ensure that the correct directory is looked up for file related test operations. This
  * is useful mainly because of the difference between IDEA and Eclipse in their treatment of 'master' and 'sub'
  * projects
- * ... IDEA uses a different project path to Eclipse (IDEA runs from the root project, 'v7') see
- * https://github.com/davidsowerby/v7/issues/253
+ * ... IDEA uses a different project path to Eclipse (IDEA runs from the root project, 'krail') see
+ * https://github.com/davidsowerby/krail/issues/253
  *
  * @author dsowerby
  */
