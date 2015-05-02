a20018d63aede7749b06e087d6f6ce973a9fda2d
David Sowerby <david.sowerby@virgin.net>
2015-05-02 11:13:26 +0100

    Added method to FileTestUtil to enable parital comparison

--- src/main/java/uk/q3c/util/testutil/FileTestUtil.java
+++ src/main/java/uk/q3c/util/testutil/FileTestUtil.java
@@ -1,5 +1,17 @@
+/*
+ * Copyright (c) 2015. David Sowerby
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package uk.q3c.util.testutil;
 
+import com.google.common.collect.Lists;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -14,7 +26,7 @@ import java.util.Optional;
 public class FileTestUtil {
 
     /**
-     * Make a line by line comparison of the text files provided at {@code file1} and {@code file2}.  If they are not
+     * Make a line by line comparison, of all lines, of the text files provided at {@code file1} and {@code file2}.  If they are not
      * the same, the comparison stops at the first mis-match and returns an Optional<String> containing a description
      * of the failure.  Lines given in {@code ignore} are not compared - this can be useful, for example, where a
      * line contains a timestamp. Even when a line is ignored for comparison, it must exist in both files for match
@@ -32,8 +44,37 @@ public class FileTestUtil {
      * @throws IOException
      */
     public static Optional<String> compare(File file1, File file2, Integer... ignore) throws IOException {
+        return compareFirst(-1, file1, file2, ignore);
+    }
+
+    /**
+     * Make a line by line comparison, up to and including {@code linesToCompare}, of the text files provided at {@code file1} and {@code file2}.
+     * <p>
+     * If they are not the same, the comparison stops at the first mis-match and returns an Optional<String> containing a description of the failure.  Lines
+     * given in {@code ignore} are not compared - this can be useful, for example, where a line contains a timestamp. Even when a line is ignored for
+     * comparison, it must exist in both files for match to succeed.
+     *
+     * @param ignore
+     *         optional lines to ignore
+     * @param file1
+     *         the first file used in comparison
+     * @param file2
+     *         the second file used in comparison
+     *
+     * @return an empty Optional if match successful, otherwise a description of the failure
+     *
+     * @throws IOException
+     */
+    public static Optional<String> compareFirst(int linesToCompare, File file1, File file2, Integer... ignore) throws IOException {
         List<String> list1 = FileUtils.readLines(file1);
         List<String> list2 = FileUtils.readLines(file2);
+
+        if (linesToCompare > 0) {
+            list1 = Lists.partition(list1, linesToCompare)
+                         .get(0);
+            list2 = Lists.partition(list2, linesToCompare)
+                         .get(0);
+        }
         List<Integer> ignores = Arrays.asList(ignore);
         boolean pass = true;
 
@@ -58,10 +99,8 @@ public class FileTestUtil {
             return Optional.of(comment);
         }
         return Optional.empty();
+
     }
 
-    public static class TestResult {
-        boolean pass;
-        String comment;
-    }
+
 }
