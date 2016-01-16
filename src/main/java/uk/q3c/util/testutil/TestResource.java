1f9e3eeffc99396689ef7bd4526b695dc4da7c20
David Sowerby <david.sowerby@virgin.net>
2016-01-16 01:35:23 +0000

    davidsowerby/krail#340 FindBugs analysis complete

--- src/main/java/uk/q3c/util/testutil/TestResource.java
+++ src/main/java/uk/q3c/util/testutil/TestResource.java
@@ -1,12 +1,14 @@
 /*
- * Copyright (c) 2015. David Sowerby
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *  * Copyright (c) 2016. David Sowerby
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ *  * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ *  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *  * specific language governing permissions and limitations under the License.
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
  */
 
 package uk.q3c.util.testutil;
@@ -14,6 +16,7 @@ package uk.q3c.util.testutil;
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -61,6 +64,8 @@ public class TestResource {
         return new File(projectRoot(), moduleName + "/src/test/java");
     }
 
+    //Not considered worth fixing in test code
+    @SuppressFBWarnings({"DLC_DUBIOUS_LIST_COLLECTION", "PATH_TRAVERSAL_IN"})
     private static File projectRoot() {
         File f = new File(".");
         Iterable<String> segments = Splitter.on("/")
