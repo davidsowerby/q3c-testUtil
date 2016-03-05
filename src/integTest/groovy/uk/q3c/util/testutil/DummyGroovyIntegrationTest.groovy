0dac0be485c25982514c3e7468c2c862d2ae303e
David Sowerby <david.sowerby@virgin.net>
2016-03-05 13:04:47 +0000

    Fix #23 Integration tests added
    Build file modified and dummy tests included

---
+++ src/integTest/groovy/uk/q3c/util/testutil/DummyGroovyIntegrationTest.groovy
@@ -0,0 +1,13 @@
+package uk.q3c.util.testutil
+
+import spock.lang.Specification
+
+/**
+ * Created by David Sowerby on 05 Mar 2016
+ */
+class DummyGroovyIntegrationTest extends Specification {
+
+    def "make it pass"() {
+        expect: true
+    }
+}
---
+++ src/integTest/java/uk/q3c/util/testutil/DummyJavaIntegrationTest.java
@@ -0,0 +1,16 @@
+package uk.q3c.util.testutil;
+
+import org.junit.Test;
+
+/**
+ * Created by David Sowerby on 05 Mar 2016
+ */
+public class DummyJavaIntegrationTest {
+
+
+    @Test
+    public void makeItPass() throws Exception {
+        assert (true);
+
+    }
+}
